dba9e94898b9a30649e1b2f9db10832f681ee3de
EC2 Default User <ec2-user@ip-172-31-13-196.ap-northeast-2.compute.internal>
2022-10-18 11:10:28 +0000

    update push ecr file

--- push-ecr.sh
+++ push-ecr.sh
@@ -1,14 +1,15 @@
-REGION=${0}
-ACCOUNT_ID=${1}
-REPOSITORY=${2}
+#!/bin/bash
+
+REGION=$1
+ACCOUNT_ID=$2
+REPOSITORY=$3
 
 ECR_URL=${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com
 
-aws ecr get-login-password --region ${REGION} | sudo docker login --username AWS --password-stdin ${ECR_URL}
+aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ECR_URL}
 
-REPOSITORY=${REPOSITORY}
-
-docker build -t ${REPOSITORY}:${TAG} ./server
+docker build -t ${REPOSITORY}:latest ./server
 
 docker tag ${REPOSITORY}:latest ${ECR_URL}/${REPOSITORY}:latest
 docker push ${ECR_URL}/${REPOSITORY}:latest
+
