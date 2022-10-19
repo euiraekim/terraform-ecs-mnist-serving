832f01ea870bdd67805443bc781d194e6d61955c
euiraekim <harrykur139@gmail.com>
2022-10-19 14:19:39 +0900

    update terraform code

--- terraform/sg/main.tf
+++ terraform/sg/main.tf
@@ -31,10 +31,11 @@ resource "aws_security_group" "alb" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
   egress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -53,15 +54,6 @@ resource "aws_security_group" "web" {
     protocol = "tcp"
     security_groups = [aws_security_group.alb.id]
   }
-
-###
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-###
 
   egress {
     from_port = 0
