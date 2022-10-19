58f6a9cdffc06116c1caffdfc4e1ed0d45417c67
euiraekim <harrykur139@gmail.com>
2022-10-19 14:11:54 +0900

    update terraform code

--- terraform/ecs/main.tf
+++ terraform/ecs/main.tf
@@ -5,3 +5,20 @@ resource "aws_ecs_cluster"  "default" {
     create_before_destroy = true
   }
 } 
+
+resource "aws_ecs_task_definition" "default" {
+  container_definitions = file("task_definitions.json")
+  family = "mnist"
+}
+
+resource "aws_ecs_service" "worker" {
+  name            = "my-service"
+  cluster         = aws_ecs_cluster.default.id
+  task_definition = aws_ecs_task_definition.default.arn
+
+  desired_count   = 2
+
+  lifecycle {
+    ignore_changes = [desired_count]
+  }
+}
